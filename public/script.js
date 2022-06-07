b08af555ac19555995366a2782e349444be7a875
Greg Gorlen <gsgorlen@gmail.com>
2022-06-06 22:22:59 -0700

    Add initial bar POC

--- public/script.js
+++ public/script.js
@@ -1,1 +1,64 @@
-console.log("hello");
+fetch("/api/assessment-results")
+  .then(res => {
+    if (!res.ok) {
+      throw Error(res.status);
+    }
+
+    return res.json();
+  })
+  .then(({data}) => {
+    const passRate = ~~((data.passed / data.total) * 100);
+    const submissionRate = ~~((data.submitted / data.total) * 100);
+    const failRate = submissionRate - passRate;
+    const notAttemptedRate = 100 - submissionRate;
+    let graph = "";
+
+    if (passRate > 0) {
+      graph += `
+        <div
+          class="stacked-bar"
+          style="background:#55df00;width:${passRate}%"
+          title="${passRate}% passed"
+        >
+          ${passRate}% passed
+        </div>
+      `;
+    }
+
+    if (failRate > 0) {
+      graph += `
+        <div
+          class="stacked-bar"
+          style="width:${failRate}%;"
+          title="${failRate}% failed"
+        >
+          ${failRate}% failed
+        </div>
+      `;
+    }
+
+    if (notAttemptedRate > 0) {
+      graph += `
+        <div
+          class="stacked-bar"
+          style="background:#fffff2;width:${notAttemptedRate}%;"
+          title="${notAttemptedRate}% not attempted"
+        >
+          ${notAttemptedRate}% not attempted
+        </div>
+      `;
+    }
+
+    document.querySelector(".stacked-bar-graph").innerHTML = graph;
+    const msg =
+      `${data.passed} of ${data.total} ` +
+      `warriors (${passRate}%) completed the mission`;
+    console.log(msg);
+    console.log(data);
+    console.log(
+      data.accumulatedSeconds.min,
+      data.accumulatedSeconds.max
+    );
+  })
+  .catch(err => console.error(err));
+
--- server.js
+++ server.js
@@ -1,10 +1,63 @@
+const axios = require("axios");
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.get("/api/assessment-results", (req, res) => {
+  const url =
+    'https://www.qualified.io/api/v1/assessments?includes[]=digest&orderBy=state DESC,title ASC&page=1&per=1&q=&where={"id":"57b63e627416b8000ed1c0f0"}';
+  // samples:
+  // 5ebb8baa5c19b1000ee16710
+  // 57b63e627416b8000ed1c0f0
+  // 5bbe36049c59930013c33783
+  // 591a602a5b75b400125be4cf
+
+  axios
+    .get(url, {
+      headers: {
+        Authorization: process.env.qualified_template_team_api_key,
+      },
+    })
+    .then(response => {
+      if (response.status !== 200) {
+        throw Error(response.status);
+      }
+
+      const {data: {data: [data]}} = response;
+      const {
+        digest: {
+          passRate,
+          submissionRate,
+          subjectStats,
+          counts: {
+            total,
+            passed,
+            states: {submitted},
+          },
+        },
+      } = data;
+      res.json({
+        data: {
+          passRate,
+          total,
+          passed,
+          submitted,
+          submissionRate,
+          accumulatedSeconds: {
+            min: subjectStats.accumulated_seconds?.points.min,
+            max: subjectStats.accumulated_seconds?.points.max,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      res.sendStatus(500);
+    });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
 
